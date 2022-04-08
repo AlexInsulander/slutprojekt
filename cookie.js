c5f8b0d81cd6078d7c09de4c2819a74570c7f0f0
Alex Insulander <alex.insulander@skola.taby.se>
2022-04-08 14:23:45 +0200

    worked on js

--- cookie.js
+++ cookie.js
@@ -6,23 +6,19 @@ class item {
   constructor(name, cost, amount, cps) {
     this.name = name;
     this.cost = cost;
-    this.amount = amount; 
+    this.amount = amount;
     this.amount = cps;
   }
 }
 
-var cursor = new item("cursors", 10, 0, 0.1)
-var grandma = new item("grandma", 100, 0, 1)
-var farm = new item("farm", 10, 0, )
-var mine = new item("mine", 10, 0)
-var factory = new item("factory", 10, 0)
-var bank = new item("bank", 10, 0)
+var cursor = new item("cursors", 10, 0, 0.1);
+var grandma = new item("grandma", 100, 0, 1);
+var farm = new item("farm", 10, 0, 10);
+var mine = new item("mine", 10, 0, 100);
+var factory = new item("factory", 10, 0, 1000);
+var bank = new item("bank", 10, 0, 10000);
 
-items = []
-
-var cps = 
-
-
+var items = [cursor, grandma, farm, mine, factory, bank];
 function buyCursor(item, itemCost) {
   if (cookies >= itemCost) {
     cookies = cookies - itemCost;
@@ -37,9 +33,9 @@ function buyCursor(item, itemCost) {
 
 function addToCookies(amount) {
   cookies = cookies + amount;
-  document.getElementById("cookies").innerHtml = cookies;
+  document.getElementById("cookies").innerHTML = cookies;
 }
 
-setInterval(function() (
+//setInterval(function() (
 
-) )
+//) )
